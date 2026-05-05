b1273c4b690f507dae1282f921a10e5e1acf4c0b
DivisorDev <divisordevelopment@gmail.com>
2026-05-05 12:11:34 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -3,7 +3,7 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 // 🔴 YAHAN APNI KEYS DAAL
 const supabase = createClient(
   'https://rajmcqxllncobghpahyc.supabase.co/rest/v1/',
-  'YOUR_ANON_KEY'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJham1jcXhsbG5jb2JnaHBhaHljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc5MTUzNTcsImV4cCI6MjA5MzQ5MTM1N30.AOzf2qjUxN9lsksC-cM7MwCosNYd60QB42g6ZT2ajpU'
 )
 
 // ================= ADD PRODUCT =================
